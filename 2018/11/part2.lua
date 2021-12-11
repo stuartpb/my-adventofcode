d836218b7233b83dc8f67964ebea1f6487ad8855
Stuart P. Bentley <s@stuartpb.com>
2021-12-11 02:18:17 +0000

    Implement slightly-better-optimized square finding algorithm

--- 2018/11/part2.lua
+++ 2018/11/part2.lua
@@ -1,30 +1,33 @@
 #! /usr/bin/env lua
 local serial = tonumber(io.read())
 local fuel = {}
+local squares = {}
 for y = 1, 300 do
   row = {}
   fuel[y] = row
+  sqrow = {}
+  squares[y] = sqrow
   for x = 1, 300 do
     local rack = x + 10
     row[x] = (rack * y + serial) * rack // 100 % 10 - 5
+    sqrow[x] = 0
   end
 end
 local largest = -math.huge
 local top, left, large
 for size = 1, 300 do
-  print('checking size',size)
   for y = 1, 301 - size do
+    local sqrow = squares[y]
     local rows = {}
     for i = 0, size - 1 do
       rows[i+1] = fuel[y+i]
     end
     for x = 1, 301 - size do
-      local square = 0
-      for rx = x, x + size - 1 do
-        for i = 1, size do
-          square = square + rows[i][rx]
-        end
+      local square = sqrow[x] + rows[size][x + size - 1]
+      for i = 0, size - 2 do
+        square = square + rows[i+1][x + size - 1] + rows[size][x+i]
       end
+      sqrow[x] = square
       if square > largest then
         largest = square
         top = y
