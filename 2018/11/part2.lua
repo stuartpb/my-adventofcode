5df16bbb58b90490eca6c2caf638bb092b6db77f
Stuart P. Bentley <s@stuartpb.com>
2021-12-11 02:52:45 +0000

    Use explicit 0 lines in summed area map

--- 2018/11/part2.lua
+++ 2018/11/part2.lua
@@ -1,14 +1,17 @@
 #! /usr/bin/env lua
 local serial = tonumber(io.read())
-local fuel = {}
+local fuel = {[0]={[0]=0}}
+for x = 1, 300 do
+  fuel[0][x] = 0
+end
 for y = 1, 300 do
-  local row = {}
+  local row = {[0]=0}
   fuel[y] = row
-  local above = fuel[y-1] or {}
+  local above = fuel[y-1]
   for x = 1, 300 do
     local rack = x + 10
-    row[x] = (rack * y + serial) * rack // 100 % 10 - 5 +
-      (row[x-1] or 0) + (above[x] or 0) - (above[x-1] or 0)
+    row[x] = (rack * y + serial) * rack // 100 % 10 - 5
+      + row[x-1] + above[x] - above[x-1]
   end
 end
 local largest = -math.huge
@@ -16,10 +19,10 @@ local top, left, large
 for size = 1, 300 do
   for y = size, 300 do
     local bottom = fuel[y]
-    local topedge = fuel[y-size] or {}
+    local topedge = fuel[y-size]
     for x = size, 300 do
-      local square = bottom[x] - (topedge[x] or 0) -
-        (bottom[x-size] or 0) + (topedge[x-size] or 0)
+      local square = bottom[x]
+        - topedge[x] - bottom[x-size] + topedge[x-size]
       if square > largest then
         largest = square
         top = y - size + 1
